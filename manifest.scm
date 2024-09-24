b5c434488bfe4e0a0935bfed882e17f0ea233520
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-09-24 23:52:56 +0800

    add manifest.scm.

---
+++ manifest.scm
@@ -0,0 +1,18 @@
+
+(use-modules (guix profiles))
+(use-modules
+ (srfi srfi-1)
+ (guix packages)
+ (guix profiles)
+ (guix transformations))
+
+(concatenate-manifests
+ (list (specifications->manifest
+        (list "clang-toolchain"
+              "valgrind"
+              "gdb"
+              "guile:debug"))
+
+       (package->development-manifest
+        (primitive-load
+         (string-append (dirname (current-filename))"/guix.scm") ))))
